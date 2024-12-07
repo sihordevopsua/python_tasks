bb25d4568c3fc13262c368d79e49e8ae10299973
“IhorSavchak“ <sihor.devops@gmail.com>
2024-12-07 23:55:56 +0200

    done

--- 1/task.py
+++ 1/task.py
@@ -1,7 +1,8 @@
+# Get data
 name = input("What is your name? ")
 age = int(input("How old are you? "))
 city = input("Where do you live? ")
-
+# Print data
 print("Hello, ", name)
 print("Your age is ", age)
 print("You live in ", city)
