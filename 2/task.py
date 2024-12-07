46c73c1b9f203d291c7324d916333a36940b29e0
“IhorSavchak“ <sihor.devops@gmail.com>
2024-12-08 00:07:47 +0200

    ready

---
+++ 2/task.py
@@ -0,0 +1,11 @@
+# Get data
+first = int(input("Enter first number: "))
+second = int(input("Enter second number: "))
+# Show result
+print("a + b = ", first+second)
+print("a - b = ", first-second)
+print("a * b = ", first*second)
+print("a / b = ", first/second)
+print("a**b = ", first**second)
+print("a//b = ", first//second)
+print("a%b = ", first%second)
